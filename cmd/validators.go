0d1e66e08823c74e5b0bce3cef1432792824aa04
Phil Tyler <phil.tyler@pantheon.io>
2023-04-04 16:26:36 -0700

    [major] validation uses positional arg instead of flag

--- cmd/validators.go
+++ cmd/validators.go
@@ -7,30 +7,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func validatorCommand(cmd *cobra.Command) error {
+func validatorCommand(cmd *cobra.Command, validatorType, filePath string) error {
 	// Is there a better way to do this? Without this we print usage on error exits.
 	// If we override at the root level, we don't get usage when we _do_ want it.
 	cmd.SilenceUsage = true
 
-	v, err := validator.ValidatorFactory(cmd.Use)
+	v, err := validator.ValidatorFactory(validatorType)
 	if err != nil {
 		return err
 	}
 
-	err = v.ValidateFromFilePath(FilePath)
+	err = v.ValidateFromFilePath(filePath)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("✨ %s.yml is valid\n", cmd.Use)
+	fmt.Printf("✨ %s is valid\n", filePath)
 	return nil
 }
 
 var sitesCommand = &cobra.Command{
-	Use:   "sites",
+	Use:   "sites path/to/file.yml",
 	Short: "validate sites.yml",
 	Long:  `Validate sites.yml`,
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return validatorCommand(cmd)
+		return validatorCommand(cmd, "sites", args[0])
 	},
 }
 
